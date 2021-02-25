30560aa90666395513d687d9415197d8d2b6e049
lars <lars_sawyer@outlook.com>
2021-02-25 07:24:19 +0000

    test

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+],
+	/**
+	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+	 */
+	function (Controller) {
+		"use strict";
+
+		return Controller.extend("dummy.project1.controller.Main", {
+			onInit: function () {
+
+			}
+		});
+	});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"dummy/project1/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
